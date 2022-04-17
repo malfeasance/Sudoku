23245dfca17fca4033989055b823996622f4965f
Al LaPointe <al@kaatbochs.com>
2022-04-16 21:22:24 -0400

    add solver code

---
+++ app/javascript/packs/solver.js
@@ -0,0 +1,12 @@
+/*
+
+class sudokuPuzzle
+
+    grid 9x9 or 81x1
+    row, column, block vs footprint
+    at each block,
+        list of possibles - empty when solved
+        fixed number flag
+        solved number flag 
+
+*/
