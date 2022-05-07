56c814e9e5cd0e713f57d69f33d2ce51ad64aa6e
Al LaPointe <al@kaatbochs.com>
2022-05-07 11:12:50 -0400

    Add restful user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "puzzles#index"
 
   resources :puzzles
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_220019) do
+ActiveRecord::Schema.define(version: 2022_05_07_144641) do
 
   create_table "puzzles", force: :cascade do |t|
     t.string "problem"
     t.string "solution"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
